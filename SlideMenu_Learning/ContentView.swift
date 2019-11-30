85fba0aafcf2ad9cb90b5a59f805880cc9c0cf9e
Ryuichi Fukami <fukaryu421@gmail.com>
2019-11-30 16:28:05 +0900

    Initial Commit

---
+++ SlideMenu_Learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlideMenu_Learning
+//
+//  Created by 深見龍一 on 2019/11/30.
+//  Copyright © 2019 深見龍一. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
